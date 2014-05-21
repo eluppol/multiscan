7050ce1afc9e89310ed2bfdcac370495d5c879a9
Ievgen Luppol <eluppol@gmail.com>
2014-05-21 16:32:59 +0300

    multiscan was rewrited

--- upload.js
+++ upload.js
@@ -1,118 +1,163 @@
-function FileUploadCtrl(scope, $html) {
-    //============== DRAG & DROP =============
-    // source for drag&drop: http://www.webappers.com/2011/09/28/drag-drop-file-upload-with-html5-javascript/
-    var dropbox = document.getElementById("dropbox")
-    scope.dropText = 'Drop files here...'
+$(function () {
+    var dropbox = $("#dropbox");
+    var fileInput = $("#file-field");
+    fileInput.on("change", function() {
+          processFile(this.files[0]);
+        });
 
-    // init event handlers
-    function dragEnterLeave(evt) {
-        evt.stopPropagation()
-        evt.preventDefault()
-        scope.$apply(function(){
-            scope.dropText = 'Drop files here...'
-            scope.dropClass = ''
-        })
-    }
-    dropbox.addEventListener("dragenter", dragEnterLeave, false)
-    dropbox.addEventListener("dragleave", dragEnterLeave, false)
-    dropbox.addEventListener("dragover", function(evt) {
-        evt.stopPropagation()
-        evt.preventDefault()
-        var clazz = 'not-available'
-        var ok = evt.dataTransfer && evt.dataTransfer.types && evt.dataTransfer.types.indexOf('Files') >= 0
-        scope.$apply(function(){
-            scope.dropText = ok ? 'Drop files here...' : 'Only files are allowed!'
-            scope.dropClass = ok ? 'over' : 'not-available'
-        })
-    }, false)
-    dropbox.addEventListener("drop", function(evt) {
-        console.log('drop evt:', JSON.parse(JSON.stringify(evt.dataTransfer)))
-        evt.stopPropagation()
-        evt.preventDefault()
-        scope.$apply(function(){
-            scope.dropText = 'Drop files here...'
-            scope.dropClass = ''
-        })
-	scope.progressVisible = false;
-        var files = evt.dataTransfer.files
-        if (files.length > 0) {
-            scope.$apply(function(){
-                scope.files = []
-                for (var i = 0; i < files.length; i++) {
-                    scope.files.push(files[i])
-                }
-            })
-        }
-    }, false)
-    //============== DRAG & DROP =============
+    dropbox.on("dragover", function(event) {
+        event.preventDefault();  
+        event.stopPropagation();
+        $(this).addClass('dragging');
+    });
 
-    scope.setFiles = function(element) {
-    scope.$apply(function(scope) {
-      console.log('files:', element.files);
-      // Turn the FileList object into an Array
-        scope.files = []
-        for (var i = 0; i < element.files.length; i++) {
-          scope.files.push(element.files[i])
-        }
-      scope.progressVisible = false
-      });
-    };
+    dropbox.on("dragleave", function(event) {
+        event.preventDefault();  
+        event.stopPropagation();
+        $(this).removeClass('dragging');
+    });
 
-    scope.uploadFile = function() {
-        var fd = new FormData()
-        for (var i in scope.files) {
-            fd.append("upload", scope.files[i])
-        }
-        var xhr = new XMLHttpRequest()
-        xhr.upload.addEventListener("progress", uploadProgress, false)
-        xhr.addEventListener("load", uploadComplete, false)
-        xhr.addEventListener("error", uploadFailed, false)
-        xhr.addEventListener("abort", uploadCanceled, false)
-        xhr.open("POST", "/api/uploads")
-        scope.progressVisible = true
-        xhr.send(fd)
-    }
+    dropbox.on("drop", function (event) {
+        event.preventDefault();  
+        event.stopPropagation();
+        processFile(event.originalEvent.dataTransfer.files[0]);
+    });
+});
 
-    function uploadProgress(evt) {
-        scope.$apply(function(){
-            if (evt.lengthComputable) {
-                scope.progress = Math.round(evt.loaded * 100 / evt.total)
+function sendFile(file) {
+    var reader = new FileReader();
+ 
+    reader.onload = function() {    
+        var xhr = new XMLHttpRequest();    
+        
+        xhr.upload.addEventListener("progress", function(e) {
+          if (e.lengthComputable) {
+            var progress = (e.loaded * 100) / e.total;
+            /* ... обновляем инфу о процессе загрузки ... */
+          }
+        }, false);
+        
+        /* ... можно обрабатывать еще события load и error объекта xhr.upload ... */
+     
+        xhr.onload = function () {
+            if(this.status == 200) {
+              /* ... все ок! смотрим в this.responseText ... */
             } else {
-                scope.progress = 'unable to compute'
+              /* ... ошибка! ... */
             }
-        })
-    }
+        };
+        xhr.open("POST", "http://avms.dit.in.ua");
 
-    function uploadComplete(evt) {
-        /* This event is raised when the server send back a response */
-	getAntiviruses();
-        var res = evt.target.response;
-	alert(res);
-	scope.file_id = res.file_id;
-    }
+        // Составляем заголовки и тело запроса к серверу,  в котором и отправим файл.
 
-    function getAntiviruses() {
-	scope.avs = [];
-	$html({
-		method : "GET",
-		url : "/api/antiviruses?limit=100",
-		success : function (response) {
-			for (var i=0; i < response.length; i++) {
-				scope.avs.push(response[i]);
-			}
-			alert(response);
-		}});
-    }	
+        var boundary = "xxxxxxxxx";
+        // Устанавливаем заголовки.
+        xhr.setRequestHeader('Content-type', 'multipart/form-data; boundary="' + boundary + '"');
+        xhr.setRequestHeader('Cache-Control', 'no-cache');
 
-    function uploadFailed(evt) {
-        alert("There was an error attempting to upload the file.")
-    }
+        // Формируем тело запроса.
+        var body = "--" + boundary + "\r\n";
+        body += "Content-Disposition: form-data; name='superfile'; filename='" + unescape( encodeURIComponent(file.name)) + "'\r\n"; // unescape позволит отправлять файлы с русскоязычными именами без проблем.
+        body += "Content-Type: application/octet-stream\r\n\r\n";
+        body += reader.result + "\r\n";
+        body += "--" + boundary + "--";
 
-    function uploadCanceled(evt) {
-        scope.$apply(function(){
-            scope.progressVisible = false
-        })
-        alert("The upload has been canceled by the user or the browser dropped the connection.")
-    }
+        // Пилюля от слабоумия для Chrome, который гад портит файлы в процессе загрузки.        
+        if (!XMLHttpRequest.prototype.sendAsBinary) {
+            XMLHttpRequest.prototype.sendAsBinary = function(datastr) {
+                function byteValue(x) {
+                    return x.charCodeAt(0) & 0xff;
+                }
+                var ords = Array.prototype.map.call(datastr, byteValue);
+                var ui8a = new Uint8Array(ords);
+                this.send(ui8a.buffer);
+            }
+        }
+
+        // Отправляем файлы.
+        if(xhr.sendAsBinary) {
+            // Только для Firefox
+            xhr.sendAsBinary(body);
+        } else {
+            // Для остальных (как нужно по спецификации W3C)
+            xhr.send(body);
+        }
+  };
+  // Читаем файл
+  reader.readAsBinaryString(file); 
+
+    return 1;
 }
 
+function processFile (file) {
+
+    var fileId = sendFile(file);
+
+    var avs, results;
+
+    function getAntiviruses() {
+        $.ajax({
+            dataType: "json",
+            url: './api/antiviruses',
+            data: {
+                'fields' : ['id', 'full_name'],
+                'active' : 1,
+                'limit' : 100,
+            },
+            success: function (antiviruses) {
+                if (antiviruses['status'] == 200) {
+                    avs = antiviruses['result'];
+                    updateTable();
+                }   
+            },
+
+            error: function (jqXHR, status, err) {
+                if (status > 500) {
+                    $(placeholder).html('<h2>ORACUL IS DEAD!</h2><br><h1>GAME OVER</h1>');
+                } else if (status == 400) {
+                    $(placeholder).html('<h2>NO DATA</h2>');
+                } else {
+                    $(placeholder).text('status: ' + jqXHR['status'] + '\nerror: ' + err + '\nResponse: ' + JSON.stringify(jqXHR));
+                }
+            }
+        });
+    }
+
+    function getResults() {
+        $.ajax({
+            dataType: "json",
+            url: './api/results',
+            data: {
+                'file_id' : 35,
+                'from' : from,
+            },
+            success: function (data) {
+                if (data['status'] == 200) {
+                   results = data['results'];
+                   updateTable();
+               }
+            },
+            error: function (jqXHR, status, err) {
+                $(placeholder).text('status: ' + jqXHR['status'] + '\nerror: ' + err + '\nResponse:' + JSON.stringify(jqXHR));
+            }
+        });
+    }
+
+    function updateTable() {
+        if (avs && results) {
+            var i = 0;
+            while (i < avs.length) {
+                var result = results.find(avs[i].id);
+                if (result) { //Wrong
+                    $("#results-table tr:last").after('<tr><td>' + av[i].full_name + '</td><td>' + result['result'] + '</td></tr>');
+                    avs.splice(i, 1);
+                } else {
+                    i++;
+                }
+            }
+            if (avs.length > 0) {
+                getResults();
+            }
+        }
+    }
+}
